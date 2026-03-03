20dc5eed5037f32d103f3012b945b40af257a0d8
ysafonau <esafronov313@gmail.com>
2026-03-03 23:51:49 +0300

    add files to my repository

---
+++ main.js
@@ -0,0 +1,20 @@
+const emojiArr = ['🐘','🦁','🐱','🐭','🐶','🐯','🐵','🐍','🦅', '🦜', '🐢', '🐱‍🐉']
+const fighterOneEl = document.getElementById('fighter-1')
+const fighterTwoEl = document.getElementById('fighter-2')
+const ringContainerEl = document.querySelector('.ring-container')
+const btnEl = document.querySelector('.btn')
+
+function generateRandomNum(){
+    const randomNum = Math.floor(Math.random() * emojiArr.length)
+    return randomNum
+}
+
+function renderGame() {
+    const fighterOne = emojiArr[generateRandomNum()]
+    const fighterTwo = emojiArr[generateRandomNum()]
+    fighterOneEl.textContent = fighterOne
+    fighterTwoEl.textContent = fighterTwo 
+    ringContainerEl.classList.remove('inactive')
+}
+
+btnEl.addEventListener('click', renderGame)
